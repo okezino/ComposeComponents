fa1c2983adffbcda081e7cc5ec9338116d33327d
okezino <okezi002@gmail.com>
2023-08-15 23:28:39 +0400

    bottom nav screen

--- app/src/main/java/com/example/composecomponent/view/bottomNav/TransactionScreen.kt
+++ app/src/main/java/com/example/composecomponent/view/bottomNav/TransactionScreen.kt
@@ -1,2 +1,45 @@
 package com.example.composecomponent.view.bottomNav
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.composecomponent.data.model.Screen
+
+@Composable
+fun TransactionScreen(navController: NavController) {
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(Color.Cyan),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Text(
+            modifier = Modifier.clickable {
+                navController.navigate(route = Screen.Home.route)
+            }, text = "TransactionScreen",
+            fontSize = MaterialTheme.typography.h4.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+
+
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun TransactionScreenPreview() {
+    TransactionScreen(navController = rememberNavController())
+}
