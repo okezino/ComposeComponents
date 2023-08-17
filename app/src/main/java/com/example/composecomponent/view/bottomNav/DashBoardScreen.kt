dcd050aabdd552499609e162c7399aa3810a88f7
okezino <okezi002@gmail.com>
2023-08-17 21:05:00 +0400

    shimmer

--- app/src/main/java/com/example/composecomponent/view/bottomNav/DashBoardScreen.kt
+++ app/src/main/java/com/example/composecomponent/view/bottomNav/DashBoardScreen.kt
@@ -1,17 +1,37 @@
 package com.example.composecomponent.view.bottomNav
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.composecomponent.data.model.Screen
@@ -19,20 +39,103 @@ import com.example.composecomponent.data.model.Screen
 @Composable
 fun DashBoardScreen() {
 
+    val shimmerColors = listOf(
+        Color.LightGray.copy(0.6f),
+        Color.LightGray.copy(0.2f),
+        Color.LightGray.copy(0.6f)
+    )
+
+    val transition = rememberInfiniteTransition()
+
+    val translateAnim = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = FastOutSlowInEasing
+            )
+        )
+    )
+
+    val brushShimmer = Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset.Zero,
+        end = Offset(x = translateAnim.value, y = translateAnim.value)
+    )
+
     Box(
-        modifier = Modifier.fillMaxSize()
-            .background(Color.Magenta),
-        contentAlignment = Alignment.Center
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Column{
+            repeat(7){
+                ShimmerGridItem(brush = brushShimmer)
+            }
+        }
+    }
+
+}
+
+
+@Composable
+fun ShimmerGridItem(brush: Brush) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(all = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
 
-        Text(text = "DashBoardScreen",
-            fontSize = MaterialTheme.typography.h4.fontSize,
-            fontWeight = FontWeight.Bold
+        Spacer(
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(brush = brush)
         )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Column() {
+
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+                    .fillMaxWidth(0.7f)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(brush)
+            )
+            Spacer(modifier = Modifier.padding(5.dp))
+
+            Spacer(
+                modifier = Modifier
+                    .height(20.dp)
+                    .fillMaxWidth(0.9f)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(brush)
+            )
+
+
+        }
 
 
     }
+}
 
+@Composable
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+fun ShimmerGridItemPreview() {
+    ShimmerGridItem(
+        brush = Brush.linearGradient(
+            listOf(
+                Color.LightGray.copy(0.6f),
+                Color.LightGray.copy(0.2f),
+                Color.LightGray.copy(0.6f)
+
+            )
+        )
+    )
 }
 
 @Composable
